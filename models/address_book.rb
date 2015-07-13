a54c90392884727d2b881210922bd0f3f53ef7fc
Natalie <nbarzila@gmail.com>
2015-07-13 17:32:58 -0400

    cheakpoint 24

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,5 +1,6 @@
 
 require_relative "entry.rb"
+require "csv"
 
 class AddressBook
   attr_accessor :entries
@@ -31,13 +32,71 @@ class AddressBook
 
   def remove_entry(entry)
     
-    @entries.each.delete('') do |entry|
+    @entries.delete() 
 
   end
 
 end
 
- require_relative "controllers/menu_controller"
+  def add_entry(name, phone, email)
+     index = 0
+     @entries.each do |entry|
+       if name < entry.name
+         break
+       end
+       index += 1
+     end
+     @entries.insert(index, Entry.new(name, phone, email))
+  end
+
+
+  def import_from_csv(file_name)
+     csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+  end
+
+   def binary_search(name)
+
+     lower = 0
+     upper = entries.length - 1
+
+ 
+     while lower <= upper
+ 
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+ 
+
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+      end
+   end
+
+
+     return nil
+   end
+  end
+   def iterative_search(name) 
+    index = 0
+  while index < entries.length
+    if name == entries[index].name
+      return entries[index]
+    end
+   index += 1
+  end
+    return nil
+
+end
+
+require_relative "controllers/menu_controller"
 
  
  menu = MenuController.new
@@ -48,34 +107,3 @@ end
  menu.main_menu
 
 end
-
-
- require "csv"
-
- class AddressBook
-   attr_accessor :entries
-
-   def initialize
-     @entries = []
-   end
-
-   def add_entry(name, phone, email)
-     index = 0
-     @entries.each do |entry|
-       if name < entry.name
-         break
-       end
-       index += 1
-     end
-     @entries.insert(index, Entry.new(name, phone, email))
-   end
-
-   def import_from_csv(file_name)
-     csv_text = File.read(file_name)
-     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
-     csv.each do |row|
-       row_hash = row.to_hash
-       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
-     end
-   end
- end
